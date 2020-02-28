c50e9d6037e92397c6d1c2e48c82f34b6a17d5e5
devopsmln2018 <43736871+devopsmln2018@users.noreply.github.com>
2020-02-28 13:16:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
  public static void main(String[] args){
-	System.out.println("======******poll from scm******======");
+	System.out.println("======******Poll from SCM******======");
   }
 }
